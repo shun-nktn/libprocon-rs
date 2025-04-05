8690bdb609ae211d6a693adbbccb4027ada9a144
Shun Nakatani <shun_nktn@hotmail.com>
2025-04-05 18:33:23 +0900

    Completed graph

---
+++ src/graph.rs
@@ -0,0 +1,55 @@
+pub struct DirectedGraph {
+    n: usize,
+    adj: Vec<Vec<usize>>,
+}
+
+impl DirectedGraph {
+    pub fn new(n: usize) -> Self {
+        Self { n, adj: vec![Vec::new(); n] }
+    }
+
+    pub fn add_edge(&mut self, u: usize, v: usize) {
+        self.adj[u].push(v);
+    }
+
+    pub fn find_sccs(&self) -> Vec<Vec<usize>> {
+        let postorder = self.postorder();
+        let transposed = self.transposed();
+        let mut result = Vec::new();
+        let mut visited = vec![false; self.n];
+        for start in postorder.into_iter().rev() {
+            let mut single_scc = Vec::new();
+            transposed.dfs(start, &mut visited, &mut single_scc);
+            result.push(single_scc);
+        }
+        result
+    }
+
+    pub fn postorder(&self) -> Vec<usize> {
+        let mut result = Vec::new();
+        let mut visited = vec![false; self.n];
+        for start in 0..self.n {
+            self.dfs(start, &mut visited, &mut result);
+        }
+        result
+    }
+
+    fn dfs(&self, current: usize, visited: &mut [bool], result: &mut Vec<usize>) {
+        if visited[current] { return; }
+        visited[current] = true;
+        for &next in &self.adj[current] {
+            self.dfs(next, visited, result);
+        }
+        result.push(current);
+    }
+
+    pub fn transposed(&self) -> Self {
+        let mut result = Self::new(self.n);
+        for v in 0..self.n {
+            for &u in &self.adj[v] {
+                result.add_edge(u, v);
+            }
+        }
+        result
+    }
+}
